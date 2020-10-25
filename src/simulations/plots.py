991530980369bb1a637b6fbd06baf87c2bb0f7f7
Isaac Shamie <isshamie@ucsd.edu>
2020-10-25 23:15:14 +0000

    sweep added. Need to debug

--- src/simulations/plots.py
+++ src/simulations/plots.py
@@ -2,9 +2,22 @@ class plots:
     def __init__(self):
         return
 
-
-
-
-class plot_ppc(plots):
+class plot_rocs(rocs):
     def __init__(self):
         return
+
+    def plot_multiple_rocs(self):
+        ## Needed for putting all curves in same space
+        base_fpr = np.linspace(0, 1, 101)
+        tprs = []
+        for roc in rocs:
+            fpr, tpr = roc[0], roc[1]
+            tpr = interp(base_fpr, fpr, tpr)
+            tpr[0] = 0.0
+            tprs.append(tpr)
+
+        tprs = np.array(tprs)
+        print(tprs)
+        print(len(tprs))
+        mean_tprs = tprs.mean(axis=0)
+        std = tprs.std(axis=0)
