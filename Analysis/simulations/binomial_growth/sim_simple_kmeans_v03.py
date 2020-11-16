a6f3152d832df56f9ba8ee4a6ce2250b437c91a6
Isaac Shamie <isshamie@ucsd.edu>
2020-11-16 06:45:07 +0000

    v03 run. Initial population made larger.

--- Analysis/simulations/binomial_growth/sim_simple_kmeans_v03.py
+++ Analysis/simulations/binomial_growth/sim_simple_kmeans_v03.py
@@ -1,16 +1,21 @@
 """ Lineage tracign simulation.
 
-Running the first version of the lineage tracing simulation.
-Files are simple_v02.yaml and wrapper_v02.yaml.
-10000 iterations, 10000 num_cells.
+Running v03 version of the lineage tracing simulation.
+Files are simple_v03.yaml and wrapper_v03.yaml.
+1000 iterations, 10000 num_cells.
+
+In this iteration, the initial population will not be limited to number
+of cells but will be its own parameter. This will affect the growth.
+
+
 Since this number is large, we are NOT going to save the full simulation
 matrices, just the results
 """
 
 from src.simulations.parametersweep import ParameterSweep
 
-default_params_f = 'parameters/simple_v02.yaml'
-sweep_params_f = 'parameters/sweep_v02.yaml'
+default_params_f = 'parameters/simple_v03.yaml'
+sweep_params_f = 'parameters/sweep_v03.yaml'
 sweep = ParameterSweep(default_params_f, sweep_params_f)
 
 sweep.run_sweep()  # Runs the simulation
