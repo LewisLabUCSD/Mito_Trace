b9e24e63fc1709c724408516a1b111bb344b3454
Isaac Shamie <isshamie@ucsd.edu>
2021-01-28 21:35:05 +0000

    Multiplex resuts for control of jan21 data

--- Analysis/multiplex/run_jan21_2021.py
+++ Analysis/multiplex/run_jan21_2021.py
@@ -5,7 +5,7 @@ from src.config import ROOT_DIR
 import os
 
 snakefile = os.path.join(ROOT_DIR, "workflows", "deconvolve_samples.Snakefile")
-out = snakemake.snakemake(snakefile=snakefile, printshellcmds=True, dryrun=True,
-                          verbose=True, configfiles=["parameters/2020_11_18.yaml"],
+out = snakemake.snakemake(snakefile=snakefile, printshellcmds=True, dryrun=False,
+                          verbose=True, configfiles=["parameters/jan21_2021.yaml"],
                           cores=20, resources={"mem_mb":100})
 print(out)
